820fcc35a20861a55030deac8b7cd7fd64134e68
Ivan Mikheykin <ivan.mikheykin@flant.com>
2018-06-05 18:34:44 +0300

    blob

--- test.sh
+++ test.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
+blob="
+aaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBB
+aaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBB
+aaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBB
+aaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBB
+aaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBB
+aaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBB
+aaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBB
+aaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBB
+aaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBB
+aaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBBaaaaaaaaaaBBBBBBBBBBB
+"
 
-for i in $(seq 1 20)
+bigblob=$(echo $blob $blob)
+
+for i in $(seq 0 20)
 do
-echo $i
+  echo $i
+  for j in $(seq 0 4)
+  do
+    echo $(( ($i * 5 + $j+1) * ${#bigblob} ))
+    echo $bigblob 1>&2
+  done
 done
